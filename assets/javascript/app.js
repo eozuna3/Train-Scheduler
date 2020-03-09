619a29fa01198efe260412bbda133274508db0c5
eozuna3 <eo30239@gmail.com>
2020-03-08 21:19:35 -0500

    Was to get information from form to populate in database and then add a new row to the current train sschedule with some of the information needed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,38 +12,68 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-var trainName = "";
-var destination = "";
-var firstTrain = "";
-var frequency = "";
 
 $("#submit-btn").on("click", function (event) {
   event.preventDefault();
+
+  //Create storage variables
+  var trainName = "";
+  var destination = "";
+  var firstTrain = "";
+  var frequency = "";
+  var momentTime = "";
   
   trainName = $("#train-name").val().trim();
   destination = $("#destination").val().trim();
-  firstTrain = moment($("#first-train").val().trim(), "HH:mm");
+  firstTrain = $("#first-train").val().trim();
+  momentObject = moment($("#first-train").val().trim(), "HH:mm");
   frequency = parseInt($("#frequency").val().trim());
 
   // Logs everything to console
   console.log(trainName);
   console.log(destination);
   console.log(firstTrain);
-  console.log(firstTrain.isValid());
+  console.log(momentObject);
+  console.log(momentObject.isValid());
+  console.log("CORRECTED TIME: " + moment(momentObject).format("HH:mm"));
   console.log(frequency);
 
+  if (!momentObject.isValid() || isNaN(frequency)) {
+    alert ("The information entered into the First Train Time and/or Frequency text boxes were not valid entries.  Please enter appropriate information into each of the text boxes.");
+    $("#first-train").val("");
+    $("#frequency").val("");
+  } else {
 
-  // database.ref().push({
-  //   trainName: trainName,
-  //   destination: destination,
-  //   firstTrain: firstTrain,
-  //   frequency: frequency,
-  //   dateAdded: firebase.database.ServerValue.TIMESTAMP
-  // });
+  database.ref().push({
+  trainName: trainName,
+  destination: destination,
+  firstTrain: firstTrain,
+  frequency: frequency,
+  dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
 
   // Clears all of the text-boxes
   $("#train-name").val("");
   $("#destination").val("");
   $("#first-train").val("");
   $("#frequency").val("");
+  }
+});
+
+database.ref().on("child_added", function(childSnapshot){
+
+// Create storage variables
+  var name = childSnapshot.val().trainName;
+  var dest = childSnapshot.val().destination;
+  var first = childSnapshot.val().firstTrain;
+  var freq = childSnapshot.val().frequency;
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(name),
+    $("<td>").text(dest),
+    $("<td>").text(freq),
+  );
+
+  $("#current-trains").append(newRow);
+
 });
