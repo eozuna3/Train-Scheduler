cf6eb2e0ebb869fceb88d1333b90662a3838de5b
eozuna3 <eo30239@gmail.com>
2020-03-13 18:40:30 -0500

    Created initial dynamic example train rows and add comments for readability.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-// Function to calculate train times as post new row
+// Function to calculate train times and post new row
 function newRow(trainName, trainDest, trainFreq, trainTime){
   var momentObject = moment(trainTime).subtract(1, "days");
   var diffTime = moment().diff(momentObject, "minutes");
@@ -45,17 +45,19 @@ newRow("Boston Bus", "Boston", 65, 0715);
 $("#submit-btn").on("click", function (event) {
   event.preventDefault();
 
-  //Create storage variables
+  //  Create storage variables
   var trainName  = "";
   var destination = "";
   var firstTrain = "";
   var frequency = "";
   
+  //  Assign user input values to the created variable
   trainName = $("#train-name").val().trim();
   destination = $("#destination").val().trim();
   firstTrain = $("#first-train").val().trim();
   frequency = parseInt($("#frequency").val().trim());
 
+  // Check to make sure all appropriate information is entered into the text-boxes
   if (!moment($("#first-train").val().trim(), "HH:mm").isValid() || isNaN(frequency) || trainName === "" || destination === "") {
     alert ("The one of the pieces of information entered into the text boxes is either missing or not valid entries.  Please enter appropriate information into each of the text boxes.");
     $("#train-name").val("");
@@ -64,6 +66,7 @@ $("#submit-btn").on("click", function (event) {
     $("#frequency").val("");
   } else {
 
+  // Push user input information to the firebase database
   database.ref().push({
   trainName: trainName,
   destination: destination,
@@ -72,7 +75,7 @@ $("#submit-btn").on("click", function (event) {
   dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 
-  // Clears all of the text-boxes
+  // Clears all of the user input text-boxes
   $("#train-name").val("");
   $("#destination").val("");
   $("#first-train").val("");
@@ -81,7 +84,6 @@ $("#submit-btn").on("click", function (event) {
 });
 
 database.ref().on("child_added", function(childSnapshot){
-
 // Create storage variables
 var name = childSnapshot.val().trainName;
 var dest = childSnapshot.val().destination;
